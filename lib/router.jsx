0c18c1a8498634f935222ef3b6088ef18b8f3521
Avishaan <avishaan@codeHatcher.com>
2016-01-26 10:54:51 -0600

    basic router placeholder components

---
+++ lib/router.jsx
@@ -0,0 +1,27 @@
+FlowRouter.route('/', {
+  name: 'Dashboard',
+  action() {
+    ReactLayout.render(App, {
+      content: <Dashboard />,
+      nav: <Nav />
+    });
+  }
+});
+FlowRouter.route('/request', {
+  name: 'RequestForm',
+  action() {
+    ReactLayout.render(App, {
+      content: <RequestForm />,
+      nav: ''
+    });
+  }
+});
+FlowRouter.route('/new-ninja', {
+  name: 'NewNinja',
+  action() {
+    ReactLayout.render(App, {
+      content: <NewNinja />,
+      nav: <Nav />
+    });
+  }
+});
